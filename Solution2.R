c3b74a104531656cf9806c13fb2e9ea2ee1f9c47
Venkata Raghu Nisanth Mallepudi <snvk444@gmail.com>
2016-04-11 00:09:07 -0700

    Added files via upload

---
+++ Solution2.R
@@ -0,0 +1,11 @@
+data1 <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", check.names=F, stringsAsFactors=F, quote='\"')
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+
+data <- subset(data1, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data1)
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
