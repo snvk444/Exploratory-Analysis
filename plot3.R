8c3c086f21aff52f4e376daa6bd914a93c5d3eb3
Venkata Raghu Nisanth Mallepudi <snvk444@gmail.com>
2016-04-11 01:09:08 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,18 +1,14 @@
-data <- read.csv("household_power_consumption.txt",header=T, sep=';', na.strings="?", 
-                 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"') 
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+data1 <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", check.names=F, stringsAsFactors=F, quote='\"')
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
 
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+data <- subset(data1, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
 
-#png("plot3.png", width=480, height=480)
-plot(datetime,subMetering1, type="l", ylab="Energy Submetering", xlab="")
-lines(datetime, subMetering2, type="l", col="red")
-lines(datetime, subMetering3, type="l", col="blue")
+png("plot3.png", width=480, height=480)
+plot(datetime,data$Sub_metering_1, type="l", ylab="Energy Submetering", xlab="", ylim = range(10,20,30,40))
+lines(datetime, data$Sub_metering_2, type="l", col="red")
+lines(datetime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 
 dev.copy(png, file="plot1.png", height=480, width=480)
